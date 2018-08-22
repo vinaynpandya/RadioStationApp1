398f04a4381bf623ee5626d7f17a8d8d7c11f2c5
Neha <Neha@Pandya_Family>
2018-08-22 15:29:55 -0500

    Added comments in Main file

--- src/main/java/com/station/demo/RadioStationApplication.java
+++ src/main/java/com/station/demo/RadioStationApplication.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class RadioStationApplication {
-
+//Main class
 	public static void main(String[] args) {
 		SpringApplication.run(RadioStationApplication.class, args);
 	}
