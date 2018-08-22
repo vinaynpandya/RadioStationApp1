30bd59fa9e11f45503dbf590f4f83250c0e1c583
Neha <Neha@Pandya_Family>
2018-08-22 15:38:11 -0500

    Commit msg test

--- src/main/java/com/station/demo/RadioStationApplication.java
+++ src/main/java/com/station/demo/RadioStationApplication.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class RadioStationApplication {
-//Main class
+//Main class Comments
 	public static void main(String[] args) {
 		SpringApplication.run(RadioStationApplication.class, args);
 	}
